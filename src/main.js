fac34b203845c9b30a8f81b99c57a664886e7e7d
christopher <tomiwa_chris@yahoo.com>
2020-08-17 10:37:35 +0100

    loaded blog title into the page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueResource)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <b-card ><h2><strong>Welcome to Weblog</strong></h2>
+    <b-list-group>
+      <b-list-group-item href="+titles.body" v-for="titles in title"
+      :key="titles.title"  :value=titles.title ><strong><h4>{{titles.title}}</h4></strong>
+      </b-list-group-item>
+  
+     
+    </b-list-group>
+     
+    <b-overlay :show="show" rounded="sm">
+    
+       
+    </b-overlay>
+  </b-card>
+   <p :disabled="show" style="cursor:pointer" variant="primary" @click="shows()">
+          See more...
+        </p>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      return {
+        show: false,
+        title: []
+        
+      }
+    },
+ created(){
+          this.$http.get('https://jsonplaceholder.typicode.com/posts')
+          .then(
+            response=>{
+                return response.json()
+            }
+        ).then(data=>{
+            this.title= data
+        })  
+      } , 
+   methods: {
+                     
+            
+      
+           
+       
+    }
+   }
 </script>
